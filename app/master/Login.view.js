1eefc7a727a2e2678203eaf96711cf14f3aafb49
frank3b <elverb@gmail.com>
2014-06-10 12:32:41 -0500

    logo

--- app/master/Login.view.js
+++ app/master/Login.view.js
@@ -17,7 +17,7 @@ sap.ui.jsview("app.master.Login", {
 			alignItems: "Center",
 			justifyContent : "Center",
 			  items: [
-			      new sap.m.Image({	src : "img/logo2.png" })
+			      new sap.m.Image({	src : "img/logo.png" })
 			  ]
 		});
 		mapPannel.addContent(ologoBox);
